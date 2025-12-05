407ace402fa0c8c87938ab956989c7e53b396276
Edward Coleridge Smith <edcoleridgesmith@gmail.com>
2025-12-05 07:39:07 +0000

    chore: create shared prettier config

---
+++ .prettierrc.mjs
@@ -0,0 +1,2 @@
+import config from './src/index.mjs'
+export default config
---
+++ src/index.mjs
@@ -0,0 +1,10 @@
+export default {
+  printWidth: 90,
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  vueIndentScriptAndStyle: false,
+  singleAttributePerLine: false,
+  plugins: ['prettier-plugin-tailwindcss'],
+}
